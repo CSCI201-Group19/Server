ed693acaf953f9aa29c9a3e91a49f452ccc8c201
Lin She <linshe@usc.edu>
2022-11-21 15:03:51 -0800

    Update UserInfo and EventInfo

--- demo2/demo2/src/main/java/project/demo2/Event/EventInfo.java
+++ demo2/demo2/src/main/java/project/demo2/Event/EventInfo.java
@@ -17,32 +17,30 @@ public class EventInfo {
     private Long id;
     private String name;
     private String location;
-    private int type;
-    private int category;
-    private LocalDate Date;
+    private String category;
+    private LocalDate date;
     private LocalTime time;
     private int capacity;
+
 
     public EventInfo() {
     }
 
-    public EventInfo(Long id, String name, String location, int type, int category, LocalDate date, LocalTime time, int capacity) {
+    public EventInfo(Long id, String name, String location, String category, LocalDate date, LocalTime time, int capacity) {
         this.id = id;
         this.name = name;
         this.location = location;
-        this.type = type;
         this.category = category;
-        Date = date;
+        this.date = date;
         this.time = time;
         this.capacity = capacity;
     }
 
-    public EventInfo(String name, String location, int type, int category, LocalDate date, LocalTime time, int capacity) {
+    public EventInfo(String name, String location, String category, LocalDate date, LocalTime time, int capacity) {
         this.name = name;
         this.location = location;
-        this.type = type;
         this.category = category;
-        Date = date;
+        this.date = date;
         this.time = time;
         this.capacity = capacity;
     }
@@ -51,60 +49,52 @@ public class EventInfo {
         return id;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public int getType() {
-        return type;
-    }
-
-    public int getCategory() {
-        return category;
-    }
-
-    public LocalDate getDate() {
-        return Date;
-    }
-
-    public LocalTime getTime() {
-        return time;
-    }
-
-    public int getCapacity() {
-        return capacity;
-    }
-
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
+    public String getLocation() {
+        return location;
+    }
+
     public void setLocation(String location) {
         this.location = location;
     }
 
-    public void setType(int type) {
-        this.type = type;
+    public String getCategory() {
+        return category;
     }
 
-    public void setCategory(int category) {
+    public void setCategory(String category) {
         this.category = category;
     }
 
+    public LocalDate getDate() {
+        return date;
+    }
+
     public void setDate(LocalDate date) {
-        Date = date;
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
     }
 
     public void setTime(LocalTime time) {
         this.time = time;
+    }
+
+    public int getCapacity() {
+        return capacity;
     }
 
     public void setCapacity(int capacity) {
@@ -117,9 +107,8 @@ public class EventInfo {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", location='" + location + '\'' +
-                ", type=" + type +
-                ", category=" + category +
-                ", Date=" + Date +
+                ", category='" + category + '\'' +
+                ", date=" + date +
                 ", time=" + time +
                 ", capacity=" + capacity +
                 '}';
