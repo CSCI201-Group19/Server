4974939be9a5a9ccaccfbfb8e5e116a74f389096
Lin She <linshe@usc.edu>
2022-11-28 21:44:22 -0800

    Update EventRegistrationRepository.java

--- demo2/demo2/src/main/java/project/demo2/Event/EventRegistrationRepository.java
+++ demo2/demo2/src/main/java/project/demo2/Event/EventRegistrationRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import project.demo2.User.UserInfo;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,6 +17,9 @@ public interface EventRegistrationRepository
 
     @Query("select er from EventRegistration er where er.user = ?1")
     List<EventRegistration> findEventRegistrationByUser(UserInfo userInfo);
+
+    @Query("select er from EventRegistration er where er.user = ?1 and er.event.date = ?2")
+    List<EventRegistration> findEventRegistrationByUserAndDate(UserInfo userInfo, LocalDate date);
 
     @Query("select er from EventRegistration er where er.user = ?1 and er.event = ?2")
     Optional<EventRegistration> findEventRegistrationByUserAndEvent(UserInfo userInfo, EventInfo eventInfo);
