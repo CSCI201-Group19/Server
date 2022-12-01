c8fbeee058a333ba2f354aa45201967e08424a25
unknown <yitianhu@usc.edu>
2022-12-01 11:04:57 -0800

    deploy

--- demo2/demo2/src/main/java/project/demo2/Event/EventInfoConfig.java
+++ demo2/demo2/src/main/java/project/demo2/Event/EventInfoConfig.java
@@ -15,13 +15,13 @@ public class EventInfoConfig {
     CommandLineRunner commandLineRunner(EventInfoRepository repository){
         return args -> {
             EventInfo studyGroup = new EventInfo("Study Group", "THH101", "study",
-                    LocalDate.of(2022, Month.NOVEMBER,15),
+                    LocalDate.of(2022, Month.DECEMBER,15),
                     LocalTime.of(10,0,0),10,1l,"Alex");
             EventInfo presentation = new EventInfo("Presentation", "SAL109", "study",
                     LocalDate.of(2022, Month.DECEMBER,20),
                     LocalTime.of(15,0,0),150, 2l, "Jason");
             EventInfo coffeeChat = new EventInfo("Coffee Chat", "StarBucks", "study",
-                    LocalDate.of(2022, Month.DECEMBER, 1),
+                    LocalDate.of(2022, Month.DECEMBER, 2),
                     LocalTime.of(20,0,0),3,3l,"Leon");
 
             repository.saveAll(List.of(studyGroup,presentation,coffeeChat));
